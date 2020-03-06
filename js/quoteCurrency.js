aa3d8fedd16fa7cf71e4775e7857631c62625c3b
Blue0x <3836882+theBlue0x@users.noreply.github.com>
2020-03-06 08:12:05 -0500

    Update quoteCurrency.js

--- js/quoteCurrency.js
+++ js/quoteCurrency.js
@@ -1,11 +1,9 @@
 let dropdown = $('#quoteCurrency');
 
-dropdown.empty();
-
 dropdown.append('<option selected="true" disabled>I would like this currency...</option>');
 dropdown.prop('selectedIndex', 0);
 
-const url = 'https://openexchangerates.org/api/currencies.json';
+const url = 'https://raw.githubusercontent.com/theBlue0x/theBlue0x.github.io/master/currencies.json';
 
 // Populate dropdown with list of provinces
 $.getJSON(url, function (data) {
