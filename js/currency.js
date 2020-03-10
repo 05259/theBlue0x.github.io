de867fdd7b66370f3a63f5e2cd6f65807ed06b06
Blue0x <3836882+theBlue0x@users.noreply.github.com>
2020-03-10 08:32:07 -0400

    Update currency.js

--- js/currency.js
+++ js/currency.js
@@ -1,28 +1,15 @@
-let dropdown = $('#baseCurrency');
 
-dropdown.append('<option selected="true" disabled>I have this currency...</option>');
+let dropdown = $('#quoteCurrency');
+
+dropdown.append('<option selected="true" disabled>I would like to exchange this currency...</option>');
 dropdown.prop('selectedIndex', 0);
 
-const url = 'https://raw.githubusercontent.com/theBlue0x/theBlue0x.github.io/master/base_currencies.json';
+const url = 'https://raw.githubusercontent.com/theBlue0x/theBlue0x.github.io/master/quote_currencies.json';
 
 // Populate dropdown with list of provinces
 $.getJSON(url, function (data) {
-  $.each(data, function (key, value) {
-    dropdown.append($('<option></option>').attr('value', value).text(value));
-  })
-});
-
-let dropdown2 = $('#quoteCurrency');
-
-dropdown2.append('<option selected="true" disabled>I would like this currency...</option>');
-dropdown2.prop('selectedIndex', 0);
-
-const url2 = 'https://raw.githubusercontent.com/theBlue0x/theBlue0x.github.io/master/quote_currencies.json';
-
-// Populate dropdown with list of provinces
-$.getJSON(url2, function (data) {
   $.each(data, function (key, value2) {
-    dropdown2.append($('<option></option>').attr('value', value2).text(value2));
+    dropdown.append($('<option></option>').attr('value', value2).text(value2));
   })
 });
 
