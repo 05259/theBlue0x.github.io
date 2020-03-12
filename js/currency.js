5f0d74129818c11869e638cfb505c771abe57146
Blue0x <3836882+theBlue0x@users.noreply.github.com>
2020-03-11 20:54:20 -0400

    Update currency.js

--- js/currency.js
+++ js/currency.js
@@ -1,15 +1,15 @@
 
-let dropdown = $('#quoteCurrency');
+let basedropdown = $('#baseCurrency');
 
-dropdown.append('<option selected="true" disabled>I would like to exchange...</option>');
-dropdown.prop('selectedIndex', 0);
+basedropdown.append('<option selected="true" disabled>I would like to exchange...</option>');
+basedropdown.prop('selectedIndex', 0);
 
-const url = 'https://raw.githubusercontent.com/theBlue0x/theBlue0x.github.io/master/quote_currencies.json';
+const baseurl = 'https://raw.githubusercontent.com/theBlue0x/theBlue0x.github.io/master/quote_currencies.json';
 
 // Populate dropdown with list of provinces
-$.getJSON(url, function (data) {
-  $.each(data, function (key, value2) {
-    dropdown.append($('<option></option>').attr('value', value2).text(value2));
+$.getJSON(baseurl, function (data) {
+  $.each(data, function (key, value) {
+    basedropdown.append($('<option></option>').attr('value', value).text(value));
   })
 });
 
