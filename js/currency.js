c785983cc4ec489f717b9cb7ccf4dea0a43e0894
Blue0x <3836882+theBlue0x@users.noreply.github.com>
2020-03-11 22:28:25 -0400

    Update currency.js

--- js/currency.js
+++ js/currency.js
@@ -6,12 +6,11 @@ dropdown.prop('selectedIndex', 0);
 const url = 'https://raw.githubusercontent.com/theBlue0x/theBlue0x.github.io/master/base_currencies.json';
 
 // Populate dropdown with list of provinces
-$.getJSON(url, function (data) {
-  $.each(data, function (key, value) {
+$.getJSON(url, function(data) {
+  $.each(data, function(key, value) {
     dropdown.append($('<option></option>').attr('value', key).text(value));
   })
 });
-
 
 let dropdown2 = $('#quoteCurrency');
 
@@ -21,8 +20,8 @@ dropdown2.prop('selectedIndex', 0);
 const url2 = 'https://raw.githubusercontent.com/theBlue0x/theBlue0x.github.io/master/quote_currencies.json';
 
 // Populate dropdown with list of provinces
-$.getJSON(url2, function (data) {
-  $.each(data, function (key2, value2) {
+$.getJSON(url2, function(data) {
+  $.each(data, function(key2, value2) {
     dropdown2.append($('<option></option>').attr('value', key2).text(value2));
   })
 });
